c8cf1ae19c22ee56892f6940afdb8187b9811ed5
chestermx <chestermx88@gmail.com>
2017-04-06 01:03:21 +0900

    [update]map.jsで複数の住所から複数のマーカーを表示する機能の追加

--- TwitterGeoGraph/static/js/map.js
+++ TwitterGeoGraph/static/js/map.js
@@ -1,17 +1,29 @@
 var map;
-var marker;
-var center = {
-    lat: 34.7019399, // 緯度
-    lng: 135.51002519999997 // 経度
-};
-function initMap() {
-    map = new google.maps.Map(document.getElementById('mainmap'), { // #sampleに地図を埋め込む
-        center: center, // 地図の中心を指定
-        zoom: 19 // 地図のズームを指定
+var geocoder;
+var addresses = [
+    '東京都千代田区永田町1丁目7-1',
+    '東京都千代田区霞が関2丁目1番1号',
+    '東京都千代田区霞が関1-1-1',
+    '東京都千代田区霞が関2-1-3'
+]
+function initMap(){
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        'address': addresses[0]
+    }, function(results, status) { // 結果
+        map = new google.maps.Map(document.getElementById('mainmap'), { // #sampleに地図を埋め込む
+            center: results[0].geometry.location, // 地図の中心を指定
+            zoom: 15 // 地図のズームを指定
+        });
     });
-
-    marker = new google.maps.Marker({ // マーカーの追加
-        position: center, // マーカーを立てる位置を指定
-        map: map // マーカーを立てる地図を指定
+    addresses.map(function(address, i){
+    	geocoder.geocode({'address' : address}, function(results, status) {
+            // マーカーの表示
+            var position = results[0].geometry.location;
+            var marker = new google.maps.Marker({
+                position: position,
+                map: map
+            });
+    	});
     });
 }
