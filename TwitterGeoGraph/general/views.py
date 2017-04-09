b8d51643bfb8b51f31036256f75c76a831fe50b6
chestermx <chestermx88@gmail.com>
2017-04-10 00:12:59 +0900

    [update]mapviewとクローラ関数の追加

--- TwitterGeoGraph/general/views.py
+++ TwitterGeoGraph/general/views.py
@@ -1,15 +1,31 @@
 from django.views import generic
 from django.conf import settings
+from django.shortcuts import redirect
 
 
 class TopView(generic.TemplateView):
     template_name = "top.html"
 
+
+class MapView(generic.TemplateView):
+    template_name = "map.html"
+
     def get_context_data(self, **kwargs):
-        context = super(TopView, self).get_context_data(**kwargs)
+        context = super(MapView, self).get_context_data(**kwargs)
         context["key"] = settings.GOOGLE_API_KEY
         return context
 
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
+def clawlerTwitter(request):
+    # ここでtwitter認証したuserのaccess_tokenを用いて、tweepyを使用してクロールする
+    # python_social_authモデルのuser_social_authsを取りたい
+    #
+    # （保留）
+    #
+    user = request.user
+    print(user)
+    return redirect('general:index')
